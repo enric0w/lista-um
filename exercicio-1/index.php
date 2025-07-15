f248220d838e6335c238cd6618e5a3e3fcd4642f
enric0w <enrico_andrade@estudante.sesisenai.org.br>
2025-07-15 11:06:51 -0300

    ex-1 da lista

--- exercicio-1/index.php
+++ exercicio-1/index.php
@@ -1,9 +1,41 @@
-<?php
-$numero = $_POST['numero_par_impar'];
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 
-if ($numero % 2 == 0) {
-    echo "O número " . $numero . " é PAR.";
-} else {
-    echo "O número " . $numero . " é ÍMPAR.";
-}
-?>
+    <form method="POST" action="">
+        <label for="digita_numero">Verifica se é par ou impar:</label>
+        <input type="number" id="digita_numero" name="digita_numero" required>
+        <button type="submit" name="numero_digitado">Verificar</button>
+   </form>
+
+
+   <?php
+
+   if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+
+    if(isset($_POST['numero_digitado'])){
+         $numero = $_POST['digita_numero'];
+         if($numero %2 == 0){
+        echo " O número  par";
+
+         }else{ 
+        echo " O número é impar";
+
+         };
+    };
+
+
+
+
+
+   };
+ ?>
+
+    
+</body>
+</html>
